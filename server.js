2bd5e3c00564f95f881b4a37d6a051c620f6589f
Vinay Gunnam <vinaygunnam@live.com>
2015-07-08 10:20:00 -0500

    Using request for simpler API.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var cors = require('cors');
-var http = require('http');
-var https = require('https');
+var request = require('request');
 var url = require('url');
 
 var app = express();
@@ -13,55 +12,12 @@ app.use(cors());
 app.options('*', cors());
 
 app.get('/resource', function resource(req, res, next) {
-  var requestedResourceUrl = req.query.url;
-  if (requestedResourceUrl) {
-    var urlObject = url.parse(requestedResourceUrl);
-    if (urlObject) {
-      var options = {
-        host: urlObject.hostname,
-        path: urlObject.path,
-        port: urlObject.port,
-        method: 'GET'
-      };
-
-      function httpCallback(response) {
-        console.log('Request started .... ' + requestedResourceUrl);
-        var str = '';
-
-        //another chunk of data has been recieved, so append it to `str`
-        response.on('data', function(chunk) {
-          str += chunk;
-        });
-
-        response.on('end', function() {
-          console.log('Resource fetched: length=' + str.length);
-          res.send(str);
-        });
-
-        response.on('error', function() {
-          res.status(500).send({
-            error: 'Error occurred while fetching the resource'
-          })
-        });
-      }
-
-      if (urlObject.protocol === 'https') {
-        https.request(options, httpCallback).end();
-      } else {
-        http.request(options, httpCallback).end();
-      }
-    } else {
-      res.status(400).send({
-        error: 'Bad resource url'
-      });
-    }
-  } else {
-    res.status(400).send({
-      error: 'Bad resource url'
-    });
-  }
+  var url = req.query.url;
+  request(url, function (error, response, body) {
+    res.status(response.statusCode).send(body);
+  });
 });
 
 app.listen(process.env.PORT || 4000, function(){
-  console.log('CORS-enabled web server listening on port ' + (process.env.PORT || 4000));
+  console.log('Server listening on port ' + (process.env.PORT || 4000));
 });
